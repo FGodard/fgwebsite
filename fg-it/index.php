2a7c2b19658270c0f2ed45bfc435db9e4fb72261
fgodard <florent_godard@hotmail.com>
2014-07-06 17:35:19 +0200

    Smarty Setup

--- fg-it/index.php
+++ fg-it/index.php
@@ -1,2 +1,13 @@
 <?php
-echo "Hello it's ".date("H:i:s");
+require_once ('./libs/smarty/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir('./view');
+$smarty->setCompileDir('./libs/smarty/compiledTemplates');
+$smarty->setCacheDir('./libs/smarty/cache');
+$smarty->setConfigDir('./libs/smarty/config');
+
+$smarty->assign('name', 'Ned');
+$smarty->display('index.tpl');
+
+// echo "Hello it's ".date("H:i:s");
