50f0f7549dbd45e88b65397853b60cfe81c9a038
fgodard <florent_godard@hotmail.com>
2014-07-08 19:30:12 +0200

    Index

--- fg-it/index.php
+++ fg-it/index.php
@@ -1,7 +1,10 @@
 <?php
-include './controller/Core.php';
+
+header('Location: ./libs/cake/webroot');
+
+// include './controller/Core.php';
 
 
-$smarty->display('index.tpl');
+// $smarty->display('index.tpl');
 
 // echo "Hello it's ".date("H:i:s");
